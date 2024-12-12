af6e5871865c45c2ff34b9a9234020afba06789f
Angel <angelprihvado@gmail.com>
2024-12-12 09:23:45 +0100

    Controlador medicamento

---
+++ src/main/java/es/uvigo/dagss/recetas/controladores/MedicamentoController.java
@@ -0,0 +1,39 @@
+package es.uvigo.dagss.recetas.controladores;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.uvigo.dagss.recetas.entidades.Farmacia;
+import es.uvigo.dagss.recetas.entidades.Medicamento;
+import es.uvigo.dagss.recetas.servicios.MedicamentoServicio;
+
+@RestController
+@RequestMapping("/api/farmacias")
+public class MedicamentoController {
+
+    private MedicamentoServicio medicamentoservicio;
+
+    @GetMapping
+    public ResponseEntity<List<Medicamento>> listarFarmacias() {
+        return ResponseEntity.ok(medicamentoservicio.listarTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<Medicamento> crearFarmacia(@RequestBody Medicamento medicamento) {
+        return ResponseEntity.ok(medicamentoservicio.guardarMedicamento(medicamento));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarFarmacia(@PathVariable Long id) {
+        medicamentoservicio.eliminarMedicamento(id);
+        return ResponseEntity.noContent().build();
+    }
+}
